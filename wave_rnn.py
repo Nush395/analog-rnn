e0c474c4770df4680ddaa004354eb416aff614d7
Nush395 <anushan395@hotmail.co.uk>
2020-06-18 22:52:37 +0100

    Add updated RNN calculation.

--- wave_rnn.py
+++ wave_rnn.py
@@ -8,23 +8,23 @@ LAP = [[0.0,  1.0,  0.0],
 
 
 class WaveLayer(tf.keras.layers.Layer):
-    def __init__(self, dt, h, b, Nx, Ny):
+    def __init__(self, dt, h, Nx, Ny):
         """Class to compute iterations of the wave equation
 
         Args:
             dt: computational time step size
             h: computational spatial step size
-            b: damping parameter
             Nx : number of x-direction grid cells in the computational domain
             Ny : number of y-direction grid cells in the computational domain
         """
         super().__init__()
         self.dt = tf.constant(dt, name='dt')
-        self.Nx = Nx
-        self.Ny = Ny
         c = np.ones([Nx, Ny])
-        self.c = tf.Variable(c, name='c', trainable=True)
-        self.b = tf.constant(b, name='b')
+        self.c_lin = tf.constant(c, name='c_lin')
+        self.c_nl = self.add_weight(shape=(Nx, Ny),
+                                    initializer='random_normal',
+                                    trainable=True)
+        # used to calculate c = c + u^2*c_nl
 
         self.laplacian = tf.constant(LAP, dtype='float32') / h**2
         # reshape to be in the format expected by the conv2d operator
@@ -48,19 +48,19 @@ class WaveLayer(tf.keras.layers.Layer):
         Parameters
         ----------
         x : Input value(s) at current time step, batched in first dimension
-        y1 : Scalar wave field one time step ago (part of the hidden state)
-        y2 : Scalar wave field two time steps ago (part of the hidden state)
+        y1 : Scalar wave field u(x,y) one time step ago (part of the hidden state)
+        y2 : Scalar wave field u(x,y) two time steps ago (part of the hidden state)
         """
         dt = self.dt
-        c = self.c
-        b = self.b
+        # c(x,y) = c_lin(x,y) + u_t(x,y)^2 * c_nl(x,y)
+        c = math.add(self.c_lin,
+                     math.multiply(math.square(y1), self.c_nl))
 
-        term_a = 2 + dt**2*math.multiply(c.pow(2), self.compute_laplacian(y1))
-
-        term_two = math.multiply(-1 - dt * b, y2)
-        denominator = dt ** (-2) + b * 0.5 * dt ** (-1)
-        y = math.multiply(denominator.pow(-1),
-                          math.add(term_a, term_two))
+        y = math.add(tf.constant(2),
+                     math.multiply(math.multiply(math.square(dt),
+                                                 math.square(c)),
+                                   self.compute_laplacian(y1)))
+        y = math.subtract(y, y2)
 
         # Insert the source
         y_out = y[:, self.src_x, self.src_y]
@@ -68,36 +68,29 @@ class WaveLayer(tf.keras.layers.Layer):
 
         return y_out, y1
 
-    def call(self, x, probes):
+    def call(self, inputs, **kwargs):
         """Propagate forward in time for the length of the input.
 
-        Parameters
-        ----------
-        x :
-            Input sequence(s), batched in first dimension
-        probe_output : bool
-            Defines whether the output is the probe vector or the entire spatial
-            distribution of the scalar wave field in time
+        Args:
+            inputs : Input sequence, batched in first dimension
         """
-        # hacky way of figuring out if we're on the GPU from inside the model
-        device = "cuda" if next(self.parameters()).is_cuda else "cpu"
-
         # First dim is batch
-        batch_size = x.shape[0]
+        batch_size = inputs.shape[0]
 
         # init hidden states
         y1 = tf.zeros([batch_size, self.Nx, self.Ny], dtype=tf.dtypes.float32)
         y2 = tf.zeros([batch_size, self.Nx, self.Ny], dtype=tf.dtypes.float32)
         y_all = []
 
-        for xi in x:
+        for xi in inputs:
+            # iterate sequence through time and collect output
             y, y1 = self.time_step(xi, y1, y2)
             y_all.append(y)
 
         y = tf.stack(y_all, axis=1)
         total_sum = 0
         y_outs = []
-        for probe_crd in probes:
+        for probe_crd in self.probes:
             px, py = probe_crd
             y_out = math.reduce_sum(math.square(y[:,:,px,py]))
             total_sum += y_out
@@ -109,13 +102,10 @@ class WaveLayer(tf.keras.layers.Layer):
 
 
 class WaveModel(tf.keras.Model):
-    def __init__(self, dt, h, b, Nx, Ny):
+    def __init__(self, dt, h, Nx, Ny):
         super().__init__()
-        self.wave_layer = WaveLayer(dt, h, b, Nx, Ny)
+        self.wave_layer = WaveLayer(dt, h, Nx, Ny)
 
-    def call(self, input, probes):
-        return self.wave_layer(input, probes)
+    def call(self, input, **kwargs):
+        return self.wave_layer(input)
 
-
-def sat_damp(u, uth=1.0, b0=1.0):
-    return b0 / (1 + math.abs(u/uth).pow(2))
